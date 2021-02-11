79ccd5b5c0b3d1d4f1e633f9159a3029cca1c614
Dromemord <art.simval@gmail.com>
2021-02-11 10:47:38 +0100

    bug fix

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -10,7 +10,7 @@ public static class SaveSystem
     {
         Debug.Log(PlayerPrefs.GetInt("Time Limit"));
         Debug.Log(PlayerPrefs.GetString("Player Name"));
-        string saveListData = SpawnScript.saveListDataChampis.Remove(Player.saveListData.Length - 1) + "],"
+        string saveListData = SpawnScript.saveListDataChampis.Remove(SpawnScript.saveListDataChampis.Length - 1) + "],"
                             + Player.saveListData.Remove(Player.saveListData.Length - 1) + "],"
                             + ItemCounter.saveListData.Remove(ItemCounter.saveListData.Length - 1) + "],"
                             + "\"Nom\":[\"" + PlayerPrefs.GetString("Player Name") + "\"],"
